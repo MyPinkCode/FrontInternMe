143a067152bb3cac563feb054d8fee1d3e46d9ab
MyPinkCode <sarra22898972@gmail.com>
2022-11-02 10:37:57 +0000

    push

---
+++ src/layouts/authentication/new-password/index.js
@@ -0,0 +1,68 @@
+/**
+=========================================================
+* InternMe React - v4.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState } from "react";
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// @mui material components
+import Switch from "@mui/material/Switch";
+
+// InternMe React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+import SoftInput from "components/SoftInput";
+import SoftButton from "components/SoftButton";
+
+// Authentication layout components
+import CoverLayout from "layouts/authentication/components/CoverLayout";
+
+// Images
+import curved9 from "assets/images/curved-images/logo.PNG";
+
+function NewPassword() {
+  const [rememberMe, setRememberMe] = useState(true);
+
+  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+
+  return (
+    <CoverLayout
+      title="Get your account back"
+      description="Get your verification code in your email"
+      image={curved9}
+    >
+      <SoftBox component="form" role="form">
+        <SoftBox mb={2}>
+          <SoftBox mb={1} ml={0.5}>
+            <SoftTypography component="label" variant="caption" fontWeight="bold">
+              Email
+            </SoftTypography>
+          </SoftBox>
+          <SoftInput type="email" placeholder="Email" />
+        </SoftBox>
+       
+        <SoftBox mt={4} mb={1}>
+          <SoftButton variant="gradient" color="info" fullWidth>
+           send
+          </SoftButton>
+        </SoftBox>
+        
+      </SoftBox>
+    </CoverLayout>
+  );
+}
+
+export default NewPassword;
---
+++ src/layouts/authentication/sign-up/index.js
@@ -0,0 +1,86 @@
+/**
+=========================================================
+* InternMe React - v4.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState } from "react";
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// @mui material components
+import Card from "@mui/material/Card";
+
+// InternMe React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+import SoftInput from "components/SoftInput";
+import SoftButton from "components/SoftButton";
+
+// Authentication layout components
+import BasicLayout from "layouts/authentication/components/BasicLayout";
+
+// Images
+import curved6 from "assets/images/curved-images/curved14.jpg";
+
+function SignUp() {
+  const [agreement, setAgremment] = useState(true);
+
+  const handleSetAgremment = () => setAgremment(!agreement);
+
+  return (
+    <BasicLayout
+      title="Welcome!"
+      description="Create new account for free."
+      image={curved6}
+    >
+      <Card>
+        <SoftBox pt={2} pb={3} px={3}>
+          <SoftBox component="form" role="form">
+            <SoftBox mb={2}>
+              <SoftInput placeholder="Name" />
+            </SoftBox>
+            <SoftBox mb={2}>
+              <SoftInput type="email" placeholder="Email" />
+            </SoftBox>
+            <SoftBox mb={2}>
+              <SoftInput type="password" placeholder="Password" />
+            </SoftBox>
+            <SoftBox mt={4} mb={1}>
+              <SoftButton variant="gradient" color="dark" fullWidth>
+                sign up
+              </SoftButton>
+            </SoftBox>
+            <SoftBox mt={3} textAlign="center">
+              <SoftTypography variant="button" color="text" fontWeight="regular">
+                Already have an account?&nbsp;
+                <SoftTypography
+                  component={Link}
+                  to="/authentication/sign-in"
+                  variant="button"
+                  color="dark"
+                  fontWeight="bold"
+                  textGradient
+                >
+                  Sign in
+                </SoftTypography>
+              </SoftTypography>
+            </SoftBox>
+          </SoftBox>
+        </SoftBox>
+      </Card>
+    </BasicLayout>
+  );
+}
+
+export default SignUp;
